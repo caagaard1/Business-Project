d91f59ba16ff4f11a0722e6922c7930983a06472
Christian Sejr Aagaard <chrsr1998@gmail.com>
2025-02-25 19:46:47 +0000

    new version

--- Code futures/Futures DA premium.py
+++ Code futures/Futures DA premium.py
@@ -66,13 +66,18 @@ on the futures contract over the day ahead price for the day, which would imply 
 pay to hedge their risk on future day-ahead prices
 """
 
-futures_adj_DApremium = (futures_adj_df.copy()).sub(futures_df_full['Day Ahead'], axis = 0)
-futures_adj_DApremium['30-day MVA'] = futures_adj_DApremium.iloc[:,0].rolling(window=30).mean()
+futures_adj_DApremium = pd.DataFrame()
+futures_adj_DApremium['M+1, adj, premium'] = (futures_adj_df.copy()).sub(futures_df_full['Day Ahead'], axis = 0)
+
+intervals = sorted(list({7, 30, 60}))
+
+#Set up of MVA of Premiums
+for i in intervals:
+    futures_adj_DApremium[f'Premium MVA {i}'] = futures_adj_DApremium.iloc[:,0].rolling(window = i).mean()
 
 plt.figure(figsize=(10, 6))
 
-plt.plot(futures_adj_DApremium.iloc[:,0], label = 'Premium')
-plt.plot(futures_adj_DApremium.iloc[:,1], label = '30-day MVA')
+plt.plot(futures_adj_DApremium, label = futures_adj_DApremium.colums)
 
 plt.title('Premium Output Over Time')
 plt.xlabel('Time Delta')
@@ -86,4 +91,60 @@ plt.show()
 having no obvious patterns except for it being very high during the period of 2022 where day-ahead prices skyrocketed. We will now attempt to
 understand the premium paid in the context of other macroeconomic factors such as interest rates (should dictate the carry in the futures
 contract), stock market returns, gas prices, gas storage levels, and economic sentiment (through producer price index or similar)."""
+
+
+
+"""Technically to be done later, but: Looking at day-ahead volatility"""
+
+#Set up day ahead prices for comparison with premium
+day_ahead_prices = pd.read_csv(r'C:\Users\chrsr\Business_Project_GitHub\Data\Day ahead\20250121 - Electricity day ahead prices, avg.csv')
+day_ahead_prices = day_ahead_prices.set_index(pd.to_datetime(day_ahead_prices['Date delivery'], format ='%d/%m/%Y')).drop(columns = {'Date delivery'})
+day_ahead_prices = day_ahead_prices.rename(columns = {'Price': 'Day ahead price'})
+day_ahead_prices = day_ahead_prices.rename_axis('Date')
+
+intervals = sorted(list({7, 30, 60}))
+
+#Set up of MVA of day-ahead prices
+for i in intervals:
+    day_ahead_prices[f'DA MVA {i}'] = day_ahead_prices['Day ahead price'].rolling(window = i).mean()
+
+# Set up of rolling vol of day-ahead prices
+for i in intervals:
+    day_ahead_prices[f'DA rolling vol {i}'] = day_ahead_prices['Day ahead price'].rolling(window=i).std()
+
+plt.figure(figsize=(10,6))
+plt.subplot(2,1,1)
+plt.plot(day_ahead_prices.iloc[:, 0:4], label = day_ahead_prices.iloc[:, 0:4].columns)
+plt.title('Day ahead prices')
+plt.legend()
+
+plt.subplot(2,1,2)
+plt.plot(day_ahead_prices.iloc[:, 4:7], label = day_ahead_prices.iloc[:, 4:7].columns)
+plt.legend()
+plt.title('Day ahead volatility')
+plt.tight_layout()
+plt.show()
+
+#Merging into a new dataframe
+
+futures_premium_reg_df = pd.merge(futures_adj_DApremium, day_ahead_prices, left_index = True, right_index = True, how ='left').dropna()
+
+correl_matrix_1 = futures_premium_reg_df.corr()
+
+"""The correlation matrix gives us valuable information:
+1. The spot premium does not seem to correlated in any way with the day-ahead price on a specific day
+2. The spot premium is somewhat correlated with the long-term moving averages and rolling volatilities of day ahead prices
+3. The moving average premiums are heavily correlated with long-term behaviors (price and volatility) of day ahead prices
+
+This would indicate that the premium on futures prices are more so dependent on the medium-term (7-60 days) market conditions which would
+make intuitive sense given that the futures contract is for an entire month, meaning that while daily price movements give information
+actors in the market are more concerned about medium-term behavior. 
+
+However, we should also be cautious in interpreting the results directly, as the correlation between day ahead prices and the premium
+is also somewhat mechanical, as the day ahead prices have been used in the calculation of the premium!!  
+"""
+
+#Simple linear regression on the variables
+
+
 
